b25432adebff9b8bcc82d755b7c40e32ad387fd8
arisha4765-source <arisha4765@ggn.hxls.org>
2026-05-25 14:21:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.get("/api/stock/:symbol", async (req, res) => {
     const symbol = req.params.symbol.toUpperCase();
 
     const response = await axios.get(
-      `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=YOUR_API_KEY`
+      `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=JXFCCD9KINZJ41OU`
     );
 
     const stock = response.data["Global Quote"];
