339b80fc8ea2822405ef71ba2464658562c74bf4
arisha4765-source <arisha4765@ggn.hxls.org>
2026-05-25 13:03:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
   res.send("Backend running");
 });
 
-app.get("/api/stock/:symbol", async (req, res) => {
+app.get("/api/stock/:symbol", (req, res) => {
   const symbol = req.params.symbol;
 
   res.json({
@@ -25,25 +25,6 @@ app.get("/api/stock/:symbol", async (req, res) => {
 const server = http.createServer(app);
 
 const wss = new WebSocket.Server({ server });
-
-wss.on("connection", (ws) => {
-  let price = 100;
-
-  const interval = setInterval(() => {
-    price += (Math.random() - 0.5) * 2;
-
-    ws.send(
-      JSON.stringify({
-        price: price.toFixed(2),
-        time: new Date().toLocaleTimeString()
-      })
-    );
-  }, 2000);
-
-  ws.on("close", () => {
-    clearInterval(interval);
-  });
-});
 
 const PORT = process.env.PORT || 5000;
 
