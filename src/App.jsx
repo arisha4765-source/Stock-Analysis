5a5bb0b2fab3f57454b9f33284c69c9f90f7b6cb
arisha4765-source <arisha4765@ggn.hxls.org>
2026-05-26 17:53:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,22 @@ export default function App() {
   const [symbol, setSymbol] = useState("");
   const [data, setData] = useState(null);
   const [aiAnswer, setAiAnswer] = useState("");
+const fetchStock = async () => {
+  if (!symbol) return;
 
+  try {
+    const res = await fetch(
+      "https://stock-analysis-81hr.onrender.com/api/stock/" +
+        symbol.toUpperCase()
+    );
+
+    const json = await res.json();
+    setData(json);
+  } catch (err) {
+    console.log(err);
+    alert("Stock not found");
+  }
+};
   return (
     <div style={{ display: "flex", height: "100vh" }}>
 
@@ -19,10 +34,12 @@ export default function App() {
           style={{ width: "100%", marginTop: 10 }}
         />
 
-        <button style={{ width: "100%", marginTop: 10 }}>
-          Search
-        </button>
-
+        <button
+  style={{ width: "100%", marginTop: 10 }}
+  onClick={fetchStock}
+>
+  Search
+</button>
         <hr />
 
         <p>📌 Watchlist</p>
@@ -48,15 +65,38 @@ export default function App() {
       {/* AI PANEL */}
       <div style={{ width: "300px", background: "#f4f4f4", padding: 20 }}>
         <h3>🤖 AI Assistant</h3>
-
+const [question, setQuestion] = useState("");
         <textarea
-          placeholder="Ask something..."
-          style={{ width: "100%", height: "100px" }}
-        />
+  value={question}
+  onChange={(e) => setQuestion(e.target.value)}
+  placeholder="Ask something..."
+  style={{ width: "100%", height: "100px" }}
+/>
+        POST /api/ai
+        const askAI = async () => {
+  try {
+    const res = await fetch(
+      "https://stock-analysis-81hr.onrender.com/api/ai",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          question,
+          symbol: data?.symbol,
+          price: data?.price,
+        }),
+      }
+    );
 
-        <button style={{ width: "100%", marginTop: 10 }}>
-          Ask AI
-        </button>
+    const json = await res.json();
+    setAiAnswer(json.answer);
+  } catch (err) {
+    setAiAnswer("AI error");
+  }
+};
+        <button style={{ width: "100%", marginTop: 10 }} onClick={askAI}>
+  Ask AI
+</button>
 
         <p>{aiAnswer}</p>
       </div>
