dd719e3ce88f03143972bc670666a76b6081dfe8
arisha4765-source <arisha4765@ggn.hxls.org>
2026-05-26 22:03:54 +0530

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,17 +1,6 @@
-import {
-  createClient,
-} from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =
-  "https://zzcizpnzamyfqmqzxasi.supabase.co";
+const supabaseUrl = "https://zzcizpnzamyfqmqzxasi.supabase.co";
+const supabaseKey = "sb_publishable_fWghn_iYnVBgKKcLAnf8Qw_BnVqvmUw";
 
-const supabaseAnonKey =
-  "sb_publishable_fWghn_iYnVBgKKcLAnf8Qw_BnVqvmUw";
-
-const supabase =
-  createClient(
-    supabaseUrl,
-    supabaseAnonKey
-  );
-
-export default supabase;
+export const supabase = createClient(supabaseUrl, supabaseKey);
