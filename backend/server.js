f1a44c1a242c3918d0c439561178ff6fc3d93fc2
arisha4765-source <arisha4765@ggn.hxls.org>
2026-05-27 14:10:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -206,6 +206,3 @@ Reason: short explanation
   }
 });
 
-// ---------------- START ----------------
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running on " + PORT));
